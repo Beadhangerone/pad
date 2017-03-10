d1647aaddd22e1b68871d18767a53bd5926ef504
Beadhangerone <denm.2206@gmail.com>
2017-03-10 23:20:14 +0200

    second presset done

--- app/controllers/presset_controller.rb
+++ app/controllers/presset_controller.rb
@@ -1,4 +1,26 @@
 class PressetController < ApplicationController
+	before_action :check_presset
+	def home
+				
+	end
+
+	def sails
+		@audios = {
+			66 => './sounds/pad1.wav',
+			69 => './sounds/wab4.wav',
+			70 => './sounds/wab2.wav',
+			71 => './sounds/pad2.wav',
+			72 => './sounds/fx2.wav',
+			75 => './sounds/hat.wav',
+			77 => './sounds/kick.wav',
+			78 => './sounds/fx1.wav',
+			79 => './sounds/snare.wav',
+			82 => './sounds/wab3.wav',
+			84 => './sounds/pad3.wav',
+			86 => './sounds/wab1.wav',
+		}
+	end
+
 	def memory
 		@audios = {
 			66 => './sounds/pad1.wav',
@@ -15,4 +37,12 @@ class PressetController < ApplicationController
 			86 => './sounds/wab1.wav',
 		}
 	end
+
+	private
+
+	def check_presset
+		presset = params[:presset]
+		redirect_to action: "#{presset}" if presset
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'presset#memory'
+  root 'presset#home'
+
+  get '/sails'=> 'presset#sails', as: 'presset_sails'
+  get '/memory'=> 'presset#memory', as: 'presset_memory'
 end
