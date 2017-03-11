52eae4304c4ff5d262184519b9038f769e9ed339
Beadhangerone <denm.2206@gmail.com>
2017-03-11 17:38:41 +0200

    second presset sounds uploaded

--- app/controllers/presset_controller.rb
+++ app/controllers/presset_controller.rb
@@ -1,40 +1,42 @@
 class PressetController < ApplicationController
 	before_action :check_presset
-	def home
-				
-	end
+
+	# recording!
+	# connect all nodes to one
+	# record this node using recorder.js
 
 	def sails
 		@audios = {
-			66 => './sounds/pad1.wav',
-			69 => './sounds/wab4.wav',
-			70 => './sounds/wab2.wav',
-			71 => './sounds/pad2.wav',
-			72 => './sounds/fx2.wav',
-			75 => './sounds/hat.wav',
-			77 => './sounds/kick.wav',
-			78 => './sounds/fx1.wav',
-			79 => './sounds/snare.wav',
-			82 => './sounds/wab3.wav',
-			84 => './sounds/pad3.wav',
-			86 => './sounds/wab1.wav',
+			77 => './sounds/sails/kick.wav',
+			# 75 => './sounds/sails/hat.wav',
+			79 => './sounds/sails/snare.wav',
+			78 => './sounds/sails/piano1.wav',
+			72 => './sounds/sails/piano2.wav',
+			66 => './sounds/sails/riff1.wav',
+			71 => './sounds/sails/riff2.wav',		
+			86 => './sounds/sails/pad1.wav',
+			70 => './sounds/sails/pad2.wav',
+			82 => './sounds/sails/pad3.wav',
+			67 => './sounds/sails/pad4.wav',
+			68 => './sounds/sails/pad5.wav',
+			69 => './sounds/sails/pad6.wav',
 		}
 	end
 
 	def memory
 		@audios = {
-			66 => './sounds/pad1.wav',
-			69 => './sounds/wab4.wav',
-			70 => './sounds/wab2.wav',
-			71 => './sounds/pad2.wav',
-			72 => './sounds/fx2.wav',
-			75 => './sounds/hat.wav',
-			77 => './sounds/kick.wav',
-			78 => './sounds/fx1.wav',
-			79 => './sounds/snare.wav',
-			82 => './sounds/wab3.wav',
-			84 => './sounds/pad3.wav',
-			86 => './sounds/wab1.wav',
+			77 => './sounds/memory/kick.wav',
+			75 => './sounds/memory/hat.wav',
+			79 => './sounds/memory/snare.wav',
+			72 => './sounds/memory/fx2.wav',
+			78 => './sounds/memory/fx1.wav',
+			66 => './sounds/memory/pad1.wav',
+			71 => './sounds/memory/pad2.wav',
+			84 => './sounds/memory/pad3.wav',
+			86 => './sounds/memory/wab1.wav',
+			70 => './sounds/memory/wab2.wav',
+			82 => './sounds/memory/wab3.wav',
+			69 => './sounds/memory/wab4.wav',
 		}
 	end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'presset#home'
+  root 'home#index'
 
   get '/sails'=> 'presset#sails', as: 'presset_sails'
   get '/memory'=> 'presset#memory', as: 'presset_memory'
