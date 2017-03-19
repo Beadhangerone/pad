439361c532cde585b9ede864704e8cfaf6afbd3c
Beadhangerone <denm.2206@gmail.com>
2017-03-19 13:50:02 +0200

    keyboard view

--- app/controllers/presset_controller.rb
+++ app/controllers/presset_controller.rb
@@ -1,6 +1,11 @@
 class PressetController < ApplicationController
 	before_action :check_presset
 
+	$keys = {
+		81 => 'q', 87 => 'w', 69 => 'e', 82 => 'r', 84 => 't', 89 => 'y', 85 => 'u', 73 => 'i', 79 => 'o', 80 => 'p',
+		65 => 'a', 83 => 's', 68 => 'd', 70 => 'f', 71 => 'g', 72 => 'h', 74 => 'j', 75 => 'k', 76 => 'l',
+		90 => 'z', 88 => 'x', 67 => 'c', 86 => 'v', 66 => 'b', 78 => 'n', 77 => 'm',
+	}
 
 	def show
 		@presset = Presset.where(name: params[:name]).take
