b0a13e07ae59a58cc31072dfd4ae06f843f25068
Beadhangerone <denm.2206@gmail.com>
2017-04-22 21:28:59 +0300

    rec btns wrapped in a class

--- public/scripts/pad.js
+++ public/scripts/pad.js
@@ -1,6 +1,5 @@
-var cLog=(x)=>console.log(x)
+var clog=(x)=>console.log(x)
 $(document).ready(function() {
-
 
 	function parseSounds(audios){
     var sounds = {}
@@ -22,38 +21,59 @@ $(document).ready(function() {
 	function startRec(btn){
 		rec.clear()
 		rec.record()
-		btn.attr('recording', 'y')
+		btn.data('rec', true)
 		btn.css('color', 'red')
 	}
 	function stopRec(btn){
 		rec.stop()
 		downloadLink()
-		btn.attr('recording', 'n')
+		btn.data('rec', false)
 		btn.css('color', 'black')
 	}
-	function recOnIt(e){
-		var btn = e.data.btn
-		var audio = e.data.audio
-		var recording = btn.attr('recording')
-		if(recording == 'n'){
-			audio.currentTime = 0
-			audio.play()
-			startRec(btn)
-		}else if(recording == 'y'){
-			stopRec(btn)
-			audio.pause()
-		}
-	}
+
 	function delRec(e){
 		var id = e.data.id
 		var div = e.data.div
-		cLog(`deleting`)
-		cLog(id)
 		div.remove()
 		for(var i in records){
 			if(records[i].id == id){
 				records.splice(i, 1)
 			}
+		}
+	}
+
+	class RecBtn{
+		constructor(audio){
+			var btn = $('<button>')
+			btn.data('rec', false)
+			if(audio){
+				btn.html('REC on it')
+				var sound = audCtx.createMediaElementSource(audio)
+				sound.connect(mainNode)
+				btn.on('click', function(){
+					var rec = $(this).data('rec')
+					if(!rec){
+						audio.currentTime = 0
+						audio.play()
+						startRec($(this))
+					}else if(rec){
+						stopRec($(this))
+						audio.pause()
+						audio.currentTime = 0
+					}
+				})
+			}else if(!audio){
+				btn.html('REC')
+				btn.on('click', function(){
+					var rec = $(this).data('rec')
+					if(!rec){
+						startRec($(this))
+					}else if(rec){
+						stopRec( $(this) )
+					}
+				})
+			}
+			return btn
 		}
 	}
 
@@ -68,26 +88,18 @@ $(document).ready(function() {
 			audio.attr('controls', 'true')
 			audio.attr('src', this.blob)
 
-			var roib = $('<button>')
-			roib.html('rec on it')
-			roib.attr('recording', 'n')
-
 			var delb = $('<i>')
 			delb.addClass('fa fa-times')
 
 			var div = $('<div>')
 			$(div).append( audio )
+			var audio = div.find('audio')[0]
+			var roib = new RecBtn(audio)
 			$(div).append( roib )
 			$(div).append(delb)
 			$('#reclist').append( div )
 
-			var audio = div.find('audio')[0]
-			var sound = audCtx.createMediaElementSource(audio)
-			sound.connect(mainNode)
-
-			roib.on('click', {btn:roib, audio:audio}, recOnIt)
 			delb.on('click', {id:this.id, div:div}, delRec)
-
 		}
 	}
 
@@ -98,7 +110,6 @@ $(document).ready(function() {
 		})
 	}
 
-
 //------------init-----------------------
 	if (!window.audCtx){window.audCtx = new AudioContext()}
 	var sounds = parseSounds(audios)
@@ -106,19 +117,12 @@ $(document).ready(function() {
 	mainNode.connect(audCtx.destination)
 	var nodes = setNodes()
 	var rec = new Recorder(mainNode)
+	var reclist = $('#reclist')
+	var mainRec = new RecBtn()
+	reclist.append(mainRec)
 	var records = []
 	var id = 1
-
 // --------------------------------------
-
-	$("#rec").on('click', function(){
-		var recording = $(this).attr('recording')
-		if(recording == 'n'){
-			startRec( $(this) )
-		}else if(recording == 'y'){
-			stopRec( $(this) )
-		}
-	})
 
 	$("body").on("keydown", function(e) {
 		var index = e.which
