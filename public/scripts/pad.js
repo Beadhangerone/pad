93d66705f92ac5ebb0d88bb491631f0bd2de6b89
Beadhangerone <denm.2206@gmail.com>
2017-04-24 17:14:36 +0300

    store records in web SQL database

--- public/scripts/pad.js
+++ public/scripts/pad.js
@@ -1,9 +1,56 @@
-var clog=(x)=>console.dir(x)
+var clog=(x)=>console.log(x)
 $(document).ready(function() {
+
+	// -------DB FUNCTIONS--------
+		function blobToBase64(blob){
+			var reader = new window.FileReader();
+			reader.onloadend = function() {
+				var res = reader.result
+				var res = res.substr(res.indexOf(',')+1)
+				pushRecToDB(res)
+			}
+			reader.readAsDataURL(blob);
+		}
+		function pushRecToDB(data){
+			db.transaction(function(tx) {
+				tx.executeSql(
+					"INSERT INTO Records (sound) values(?)", [data],
+					null, null
+				);
+			});
+		}
+
+		function base64ToBlob(b64Data){
+			var byteCharacters = atob(b64Data);
+			var byteNumbers = new Array(byteCharacters.length);
+			for (var i = 0; i < byteCharacters.length; i++) {
+			    byteNumbers[i] = byteCharacters.charCodeAt(i);
+			}
+			var byteArray = new Uint8Array(byteNumbers);
+			var blob = new Blob([byteArray], {type: 'audio/waw'});
+			return blob
+		}
+		function getStoredRecs(){
+			db.transaction(function(tx) {
+				tx.executeSql(
+					"SELECT * FROM Records", [],
+					function(tx, result) {
+						for(var i = 0; i < result.rows.length; i++) {
+							var id = result.rows.item(i)['id']
+							var b64data = result.rows.item(i)['sound']
+							var blob = base64ToBlob(b64data)
+							var record = new Record(blob)
+						}
+					},
+					null
+				);
+			});
+		}
+	// ------_DB FUNCTIONS--------
 
 	function parseSounds(audios){
     var sounds = {}
-    for(key in audios){
+    for(var key in audios){
       sounds[key] = new Audio(audios[key])
     }
     return sounds
@@ -11,19 +58,11 @@ $(document).ready(function() {
 
 	function setNodes(){
 		var nodes = {}
-		for (key in sounds){
+		for(var key in sounds){
 			nodes[key] = new MediaElementAudioSourceNode(audCtx, { mediaElement: sounds[key] })
 			nodes[key].connect(mainNode)
 		}
 		return nodes
-	}
-
-	function stSet(name, val){
-		sessionStorage.setItem(name, JSON.stringify(val))
-	}
-	function stAsk(name, defaul){
-		var result = JSON.parse( sessionStorage.getItem(name) )
-		return result?result:defaul
 	}
 
 	function startRec(btn){
@@ -38,25 +77,16 @@ $(document).ready(function() {
 		btn.data('rec', false)
 		btn.css('color', 'black')
 	}
-	function downloadLink(){
-		rec.exportWAV(function(blob) {
-			var id = stAsk('current_id', 1)
-			var newRec = new Record(blob)
-			records.push(newRec)
-			stSet('records', records)
-			stSet('current_id', id+1)
-		})
-	}
 	function delRec(e){
 		var id = e.data.id
 		var div = e.data.div
 		div.remove()
-		for(var i in records){
-			if(records[i].id == id){
-				records.splice(i, 1)
-				stSet('records', records)
-			}
-		}
+	}
+	function downloadLink(){
+		rec.exportWAV(function(blob) {
+			new Record(blob)
+			blobToBase64(blob)
+		});
 	}
 
 	class RecBtn{
@@ -77,7 +107,7 @@ $(document).ready(function() {
 						audio.pause()
 						audio.currentTime = 0
 					}
-				})
+				});
 			}else if(!audio){
 				btn.html('REC')
 				btn.on('click', function(){
@@ -85,9 +115,9 @@ $(document).ready(function() {
 					if(!rec){
 						startRec($(this))
 					}else if(rec){
-						stopRec( $(this) )
+						stopRec($(this))
 					}
-				})
+				});
 			}
 			return btn
 		}
@@ -96,7 +126,8 @@ $(document).ready(function() {
 		constructor(blob,controls=true){
 			var audio = $('<audio>')
 			audio.attr('controls', controls)
-			audio.attr('src', blob)
+			var src = window.URL.createObjectURL(blob)
+			audio.attr('src', src )
 			return audio[0]
 		}
 	}
@@ -111,13 +142,8 @@ $(document).ready(function() {
 
 	class Record{
 		constructor(blob){
-			var id = stAsk('current_id', 1)
 			var div = $('<div>')
-			this.id = id
-			this.blob = URL.createObjectURL(blob)
-			// this.time = new Date().toISOString()
-			// this.parent = parent
-
+			this.blob = blob
 			var audio = new Aud(this.blob)
 			var roib = new RecBtn(audio)
 			var delb = new Delb(this.id, div)
@@ -125,10 +151,8 @@ $(document).ready(function() {
 			$(div).append(roib)
 			$(div).append(delb)
 			$('#reclist').append( div )
-
 		}
 	}
-
 
 //------------init-----------------------
 	if (!window.audCtx){window.audCtx = new AudioContext()}
@@ -140,21 +164,18 @@ $(document).ready(function() {
 	var reclist = $('#reclist')
 	var mainRec = new RecBtn()
 	reclist.append(mainRec)
-	var records = stAsk('records', [])
-	for(var i in records){
-		var record = records[i]
-		var div = $('<div>')
-		clog(record.blob)
-		var audio = new Aud(record.blob)
-		var roib = new RecBtn(audio)
-		var delb = new Delb(record.id, div)
-
-		$(div).append(audio)
-		$(div).append(roib)
-		$(div).append(delb)
-		$('#reclist').append( div )
-	}
-// --------------------------------------
+	// -------INIT DB--------
+	db = openDatabase("Recs", "0.1", "Records", 200000);
+	if(!db){alert("Failed to connect to database. Try to refresh the page.");}
+	db.transaction(function(tx){
+		tx.executeSql(
+			"CREATE TABLE IF NOT EXISTS Records (id INTEGER PRIMARY KEY, sound TEXT)", [],
+			null,null
+		);
+	})
+	// ------_INIT DB--------
+	getStoredRecs()
+	//-----------_init-----------------------
 
 	$("body").on("keydown", function(e) {
 		var index = e.which
